0504200737060a66c00709af94f4caa2c58d7415
Resul023 <73m6u7h@code.edu.az>
2022-05-10 23:57:45 -0700

    HomeWork

---
+++ main.js
@@ -0,0 +1,47 @@
+// let arr = [1,55,60,6,5];
+
+// console.log(findBigNum(arr));    
+
+// function findBigNum(array){
+//     let result;
+//     for(i=0;i<array.length;i++){
+//         for(j=0;j<array.length;j++)
+//         {
+//             if(array[j]>array[i]){
+
+//                 array[i]=array[j]
+//                 result =array[j];  
+//             }
+//         }
+//     }
+//     return result; 
+// }        Task1
+
+// let arr = [1,22,7,6,8];
+
+// console.log(findOdd(arr))
+
+// function findOdd(array){
+//     let sum=0;
+//     for(i=0;i<array.length;i++){
+//         if(array[i] % 2==0){
+//             sum+=array[i];
+//         }
+//     }
+//     return sum;
+// }                            TASK2
+
+
+// let numberOne = prompt("Enter number one:");
+// let numberTwo = prompt("Enter number two:");
+
+// function numberKv(numOne,numTwo){
+
+//     for(i=0;i<numTwo;i++){
+//         numOne*=numOne;
+//         i++;
+//     }
+//     return numOne
+// }
+
+// console.log(numberKv(numberOne,numberTwo)) TASK3
